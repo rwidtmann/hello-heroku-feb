720a85944aef6451a84ff6e85a5bf358ef7e775b
Richard Widtmann <rwidtmann@sbcglobal.net>
2022-02-18 10:58:10 -0600

    First Commit

---
+++ src/main/java/com/company/HelloHerokuFeb/HelloHerokuFebApplication.java
@@ -0,0 +1,13 @@
+package com.company.HelloHerokuFeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHerokuFebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHerokuFebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/HelloHerokuFeb/HelloHerokuFebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.HelloHerokuFeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHerokuFebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
